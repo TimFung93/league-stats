eebec7bb2cc356c295335d880de118d66976bf38
timfung93 <timothy.fung.04@gmail.com>
2017-07-06 16:10:35 -0400

    update view, added routes

---
+++ champ_stats/src/Components/Header.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+
+
+
+
+export default class Header extends Component {
+
+
+
+  render() {
+  
+     
+
+     
+
+    return (
+      <div className="App">
+        <h1>You have reached Header</h1>
+
+      </div>
+    );
+  }
+}
+
--- champ_stats/src/index.js
+++ champ_stats/src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Header from './Components/Header'
 
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+	<Router history={Router} >
+		<Route path='/' component={Header} />
+	</Router>
+), document.getElementById('root'))
+
 
